3b70e0a69893b9388d4d575641c9fc4364923814
Somshubra Majumdar <titu1994@gmail.com>
2016-08-24 23:32:37 +0530

    Updated model.py with some minor changes

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ out = Dense(7, activation='softmax')(x)
 model = Model(ip, out)
 
 model.summary()
-plot(model, 'mini-vgg.png', show_shapes=True, show_layer_names=True)
+#plot(model, 'mini-vgg.png', show_shapes=True, show_layer_names=True)
 
 model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['acc'])
 print('Compiling model.')
@@ -38,7 +38,7 @@ trainPath = r"C:\Shape Dataset\train"
 testPath = r"C:\Shape Dataset\test"
 
 batchSize = 128
-nbEpoch = 100
+nbEpoch = 10
 
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -55,7 +55,7 @@ model.load_weights('Model Weights.h5')
 
 #model.fit_generator(train_generator, samples_per_epoch=350000, nb_epoch=nbEpoch,
 #                    callbacks=[ModelCheckpoint('Model Weights.h5', monitor='val_acc', save_best_only=True, save_weights_only=True)],
-#                    validation_data=test_generator, nb_val_samples=70000, nb_worker=4)
+#                    validation_data=test_generator, nb_val_samples=70000)
 
 scores = model.evaluate_generator(test_generator, 70000)
 print(scores)
